5d4c6d978da0fa18a321e7b144ad26d737e609a1
NicolasRengifo24 <nicolaseggo@gmail.com>
2025-11-03 10:47:52 -0500

    docs: definicion de constantes , adicion del atributo integrity para el cdn de boostrap y eliminacin de comentario

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,9 @@
     <title>@yield('title', 'Dulcería - Sistema de Gestión')</title>
     
    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     
-    
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" integrity="sha384-CXXomfNnPV4Y+45i8D1+/xSx7xWc6HvNe1E8+y+0TXkHuHp50f1m4CiCgQJcELd/" crossorigin="anonymous">
     
     
     <style>
@@ -131,10 +130,8 @@
         </div>
     </footer>
 
-    <!-- Bootstrap 5 JS Bundle -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     
-    <!-- Scripts personalizados -->
     @yield('scripts')
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
+define('LOGIN_ROUTE', '/login');
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriaProductoController;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return redirect('/login');
+    return redirect(LOGIN_ROUTE);
 });
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+Route::get(LOGIN_ROUTE, [AuthController::class, 'showLoginForm'])->name('login');
+Route::post(LOGIN_ROUTE, [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
